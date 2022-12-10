eba1443918bd204729fa1dab002e1452523df085
Radoslaw Statkiewicz <rstat8777@gmail.com>
2022-12-10 16:02:05 -0500

    6) I created a function with queryselector id password and variable prompts1 that will pick up prompts from users. Then i aded if prompts1 correct will call a variable password that is assigned to generatePassword  and  value of passwordtext assigned to a password

--- challenge3-rstat/Develop/script.js
+++ challenge3-rstat/Develop/script.js
@@ -10,12 +10,13 @@ var generateBtn = document.querySelector("#generate");//4) JS chooses id of a bu
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+ 
   var passwordText = document.querySelector("#password");
-  
-  
-
-  passwordText.value = password;
+  var prompts1 = getPrompts(); 
+  if(prompts1){//6) I created a function with queryselector id password and variable prompts1 that will pick up prompts from users. Then i aded if prompts1 correct will call a variable password that is assigned to generatePassword  and  value of passwordtext assigned to a password 
+    var password = generatePassword();
+    passwordText.value = password;
+  }
  else {
   passwordText.value = "";//5) it will be blank when false. i added else for passworText.value to make it blank when false. There was a function prepared previously that contained a variable apssword that was assigned to generate a password and the second variable in the function was passwordText that was assigned to pick id password from html that was added in textarea"
 }
